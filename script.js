2edd12d36cf397429ae9496e41154acadb2b73bf
Nikhil Rawat <117645790+nikhil-rawat02@users.noreply.github.com>
2023-05-21 12:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+const tbody = document.getElementsByTagName("tbody")[0];
+const td = document.querySelectorAll("tbody tr");
+let sum = 0;
+for(let i =0; i < td.length; i++){
+	 sum += parseInt(td[i].children[2].innerText);
+}
+const newTd = document.createElement("tr");
+newTd.innerText = sum;
+newTd.setAttribute('data-ns-test', 'grandTotal');
+
+tbody.appendChild(newTd);
